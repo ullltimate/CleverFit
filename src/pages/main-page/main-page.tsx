b717713eb450a996f45a1a976f3e431ba0bc49ca
ullltimate <ullltimate.zp@gmail.com>
2024-02-11 17:08:38 +0300

    fix: fix styles

--- src/pages/main-page/main-page.tsx
+++ src/pages/main-page/main-page.tsx
@@ -31,50 +31,54 @@ export const MainPage: React.FC = () => {
 
     return (
         <>
-            <Layout>
-                <SiderBar />
-                <Layout className='site-layout'>
-                    <Header />
-                    <Content style={{ margin: 24 }}>
-                        <Card bordered={false} className='content-discription'>
-                            <p className='content-text'>
-                                С CleverFit ты сможешь: <br />
-                                — планировать свои тренировки на календаре, выбирая тип и уровень
-                                нагрузки;
-                                <br />
-                                — отслеживать свои достижения в разделе статистики, сравнивая свои
-                                результаты с нормами и рекордами;
-                                <br />
-                                — создавать свой профиль, где ты можешь загружать свои фото, видео и
-                                отзывы о тренировках;
-                                <br />— выполнять расписанные тренировки для разных частей тела,
-                                следуя подробным инструкциям и советам профессиональных тренеров.
-                            </p>
-                        </Card>
-                        <Card bordered={false} className='content-discription'>
-                            <h4 className='content-subtitle'>
-                                CleverFit — это не просто приложение, а твой личный помощник в мире
-                                фитнеса. Не откладывай на завтра — начни тренироваться уже сегодня!
-                            </h4>
-                        </Card>
-                        <div className='cards-wrapper'>
-                            <Row gutter={16} className='wrap'>
-                                {contentCards.map(
-                                    (e: ICardInfo, i: number): React.ReactNode => (
-                                        <ContentCard
-                                            key={i}
-                                            title={e.title}
-                                            btnText={e.btnText}
-                                            btnIcon={e.btnIcon}
-                                        />
-                                    ),
-                                )}
-                            </Row>
-                        </div>
-                    </Content>
-                    <Footer />
+            <div className='main-wrapper wrapper'>
+                <Layout>
+                    <SiderBar />
+                    <Layout className='site-layout'>
+                        <Header />
+                        <Content style={{ margin: 24 }}>
+                            <Card bordered={false} className='content-discription'>
+                                <p className='content-text'>
+                                    С CleverFit ты сможешь: <br />
+                                    — планировать свои тренировки на календаре, выбирая тип и
+                                    уровень нагрузки;
+                                    <br />
+                                    — отслеживать свои достижения в разделе статистики, сравнивая
+                                    свои результаты с нормами и рекордами;
+                                    <br />
+                                    — создавать свой профиль, где ты можешь загружать свои фото,
+                                    видео и отзывы о тренировках;
+                                    <br />— выполнять расписанные тренировки для разных частей тела,
+                                    следуя подробным инструкциям и советам профессиональных
+                                    тренеров.
+                                </p>
+                            </Card>
+                            <Card bordered={false} className='content-discription'>
+                                <h4 className='content-subtitle'>
+                                    CleverFit — это не просто приложение, а твой личный помощник в
+                                    мире фитнеса. Не откладывай на завтра — начни тренироваться уже
+                                    сегодня!
+                                </h4>
+                            </Card>
+                            <div className='cards-wrapper'>
+                                <Row gutter={16} className='wrap'>
+                                    {contentCards.map(
+                                        (e: ICardInfo, i: number): React.ReactNode => (
+                                            <ContentCard
+                                                key={i}
+                                                title={e.title}
+                                                btnText={e.btnText}
+                                                btnIcon={e.btnIcon}
+                                            />
+                                        ),
+                                    )}
+                                </Row>
+                            </div>
+                        </Content>
+                        <Footer />
+                    </Layout>
                 </Layout>
-            </Layout>
+            </div>
         </>
     );
 };
