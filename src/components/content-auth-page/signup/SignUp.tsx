9d186f26fa8741fe7c665b1d1fd46f62ae09ee9e
ullltimate <ullltimate.zp@gmail.com>
2024-02-17 01:51:22 +0300

    feat: add sign up layout

--- src/components/content-auth-page/signup/SignUp.tsx
+++ src/components/content-auth-page/signup/SignUp.tsx
@@ -1,9 +1,47 @@
+import { GooglePlusOutlined } from '@ant-design/icons';
+import { Form, Input, Button } from 'antd';
 import React from 'react';
+import './signUp.css'
 
 export const SignUp: React.FC = () => {
     return (
         <>
-            SignUp
+            <Form
+                name='normal_registration'
+                className='registration-form'
+                initialValues={{ remember: true }}
+            >
+                <Form.Item
+                    name='username'
+                    rules={[{ required: true, message: 'Please input your Username!' }]}
+                >
+                    <Input addonBefore='e-mail' />
+                </Form.Item>
+                <Form.Item
+                    name='password'
+                    rules={[{ required: true, message: 'Please input your Password!' }]}
+                >
+                    <Input.Password type='password' placeholder='Пароль' />
+                </Form.Item>
+                <Form.Item
+                    name='repeat-password'
+                    rules={[{ required: true, message: 'Please input your Password!' }]}
+                >
+                    <Input.Password type='password' placeholder='Повторите пароль' />
+                </Form.Item>
+
+                <Form.Item>
+                    <Button type='primary' htmlType='submit' className='login-form-button'>
+                        Войти
+                    </Button>
+                </Form.Item>
+
+                <Form.Item>
+                    <Button type='text' className='login-form-button'>
+                        <GooglePlusOutlined /> Войти через Google
+                    </Button>
+                </Form.Item>
+            </Form>
         </>
     );
 };
