b32e3b7d2889f33c468a4105d435711f6a5568a7
ullltimate <ullltimate.zp@gmail.com>
2024-02-19 20:18:03 +0300

    fix: fix loader

--- src/components/loader/Loader.tsx
+++ src/components/loader/Loader.tsx
@@ -1,19 +1,12 @@
 import React from 'react';
-import Lottie from 'react-lottie';
+import Lottie from 'lottie-react';
 import animationData from './lottie.json';
 
 export const Loader: React.FC = () => {
-    const defaultOptions = {
-        loop: true,
-        autoplay: true,
-        animationData: animationData,
-        rendererSettings: {
-            preserveAspectRatio: 'xMidYMid slice',
-        },
-    };
+    
     return (
         <>
-            <Lottie options={defaultOptions} height={150} width={150} />
+            <Lottie animationData={animationData} loop={true} style={{width: '150px'}} />
         </>
     );
 };
