15cfc4b7cbdd1894b8fd51b205e6b8c939575916
ullltimate <ullltimate.zp@gmail.com>
2024-02-28 19:00:46 +0300

    feat: add link to comment page

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -3,16 +3,20 @@ import { Row, Col, Button, Card } from 'antd';
 import { AndroidFilled, AppleFilled } from '@ant-design/icons';
 
 import './footer.css';
+import { useNavigate } from 'react-router-dom';
+import { PATHS } from '@constants/paths';
 
 const { Meta } = Card;
 
 export const Footer: React.FC = () => {
+    const navigate = useNavigate();
+
     return (
         <>
             <footer className='footer'>
                 <Row justify={'space-between'} wrap={true} style={{ flexWrap: 'wrap-reverse' }}>
                     <Col flex='none'>
-                        <Button type='link'>Смотреть отзывы</Button>
+                        <Button type='link' onClick={() => navigate(PATHS.FEEDBACKS)} data-test-id='see-reviews'>Смотреть отзывы</Button>
                     </Col>
                     <Col flex='none' className='footer-card'>
                         <Card
