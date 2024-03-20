af87106e6d1a0a5c785190b455fb2a4174234d7c
ullltimate <ullltimate.zp@gmail.com>
2024-03-21 00:41:07 +0300

    feat: add setting eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,14 +2,39 @@ module.exports = {
     root: true,
     env: { browser: true, es2020: true },
     extends: [
-        'eslint:recommended',
-        'plugin:@typescript-eslint/recommended',
-        'plugin:react-hooks/recommended',
+        require.resolve('arui-presets-lint/eslint')
     ],
-    ignorePatterns: ['dist', '.eslintrc.cjs', 'stylelint.config.cjs', 'coverage'],
+    ignorePatterns: ['dist', 'coverage', '*.cjs'],
     parser: '@typescript-eslint/parser',
+    parserOptions: {
+        project: ['./tsconfig.eslint.json', './cypress/tsconfig.json'],
+    },
     plugins: ['react-refresh'],
+    overrides: [
+        {
+            files: ['cypress/**/*.ts'],
+            rules: {
+                'cypress/no-unnecessary-waiting': 'off',
+            },
+        },
+        {
+            files: ['src/redux/**/*.ts'],
+            rules: {
+                'no-param-reassign': 'off',
+                'no-return-assign': 'off',
+                'import/no-default-export': 'off',
+            },
+        },
+    ],
     rules: {
         'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+        'import/no-absolute-path': 'off',
+        'react/react-in-jsx-scope': 'off',
+        'import/no-extraneous-dependencies': [
+            'error',
+            {
+                devDependencies: ['cypress/**/*.ts', '/*.test.{ts,tsx,js,jsx}'],
+            },
+        ]
     },
 };
